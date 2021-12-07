f255b379792ba0e3c6cab9a81124c15997a6ddaa
cainpalmer <cain.a.palmer@gmail.com>
2021-12-07 16:07:41 -0500

    completed routers

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,38 @@
-// DO YOUR MAGIC
+
+// Router Variables
+const router = require('express').Router()
+const Car = require('./cars-model')
+const {checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid} = require('./cars-middleware')
+
+// Router Requests
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
+})
+
+router.post('/', checkCarPayload, checkVinNumberUnique, checkVinNumberValid, async (req, res, next) => {
+    try {
+        const newCar = await Car.create(req.body)
+        res.status(201).json(newCar)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+// Exports
+module.exports = router
