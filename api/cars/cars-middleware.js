734735084dfde4196290e7f6fe71c4e389915fef
cainpalmer <cain.a.palmer@gmail.com>
2021-12-07 18:08:31 -0500

    bugs in code

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -4,66 +4,62 @@ const Car = require('./cars-model')
 const vin = require('vin-validator')
 
 // Middlewares
-const checkCarId = async (req, res, next) => {
-  try {
-    const car = await Car.getById(req.params.id)
-    if (!car) {
-      next({status: 404, message: 'failed to retrieve'})
-    } else {
-      req.car = car
-      next()
-    }
-  } catch(error) {
-    next(error)
-  }
+const checkCarId = (req, res, next) => {
+  const { id } = req.params;
+  Car.getById(id)
+    .then(foundCar => {
+      if (!foundCar) {
+        res.status(404).json({ message: `car with id ${id} is not found` })
+      }
+      else {
+        next()
+      }
+    })
 }
 
 const checkCarPayload = (req, res, next) => {
-  if (!req.body.vin) return next({
-    status: 400,
-    message: 'missing vin'
-  })
-  if (!req.body.make) return next({
-    status: 400,
-    message: 'missing make'
-  })
-  if (!req.body.model) return next({
-    status: 400,
-    message: 'missing model'
-  })
-  if (!req.body.mileage) return next({
-    status: 400,
-    message: 'missing mileage'
-  })
-  next()
-}
-
-const checkVinNumberValid = async (req, res, next) => {
-  if (vin.validate(req.body.vin)) {
-    next()
+  // DO YOUR MAGIC
+  if (!req.body.vin) {
+    res.status(400).json({ message: 'vin is missing!' })
+  } else if (!req.body.make) {
+    res.status(400).json({ message: 'make is missing!' })
+  } else if (!req.body.model) {
+    res.status(400).json({ message: 'model is missing!' })
+  } else if (!req.body.mileage) {
+    res.status(400).json({ message: 'mileage is missing!' })
   } else {
-    next({
-      status: 400,
-      message: 'vin is invalid'
-    })
+    next()
   }
 }
 
-const checkVinNumberUnique = async (req, res, next) => {
-  try {
-    const exists = await Cars.getByVin(req.body.vin)
-    if (!exists) {
-      next()
-    } else {
-      next({
-        status: 400,
-        message: 'vin already exists'
-      })
-    }
-  } catch(err) {
-    next(err)
+const checkVinNumberValid = (req, res, next) => {
+  // DO YOUR MAGIC
+  let isVinValid = vinValidator.validate(req.body.vin);
+  if (!isVinValid) {
+    res.status(400).json({ message: `vin ${req.body.vin} is invalid` })
+  }
+  else {
+    next()
   }
 }
+
+const checkVinNumberUnique = (req, res, next) => {
+  // DO YOUR MAGIC
+  Car.checkVinUnique(req.body.vin)
+    .then(response => {
+      if (response) {
+        res.status(400).json({ message: `vin ${req.body.vin} already exists` })
+      }
+      else {
+        next()
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message })
+    })
+
+}
+
 
 // Exports
 module.exports = {checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid}
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -5,26 +5,36 @@ const Car = require('./cars-model')
 const {checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid} = require('./cars-middleware')
 
 // Router Requests
-router.get('/', async (req, res, next) => {
-    try {
-        const cars = await Car.getAll()
-        res.status(200).json(cars)
-    } catch(error) {
-        next(error)
-    }
+router.get('/api/cars', (req, res) => {
+    Car.getAll()
+        .then(allCars => {
+            res.status(200).json(allCars)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+        })
+    console.log('getting all cars')
 })
 
-router.get('/:id', checkCarId, async (req, res, next) => {
-    res.json(req.car)
+router.get('/api/cars/:id', checkCarId, (req, res) => {
+    const { id } = req.params;
+    Car.getById(id)
+        .then(car => {
+            res.status(200).json(car)
+        })
+        .catch(err => {
+            res.status(400).json({ message: err.message })
+        })
 })
 
-router.post('/', checkCarPayload, checkVinNumberUnique, checkVinNumberValid, async (req, res, next) => {
-    try {
-        const car = await Car.create(req.body)
-        res.status(201).json(car)
-    } catch (error) {
-        next(error)
-    }
+router.post('/api/cars', checkVinNumberUnique, checkVinNumberValid, checkCarPayload, (req, res) => {
+    Car.create(req.body)
+        .then(newCar => {
+            res.status(201).json(newCar)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+        })
 })
 
 // Exports
